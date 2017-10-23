ebfc6c91158b459fe9dd2b55a94260826e5cd0dd
Gabriel <ionescuig@yahoo.com>
2017-10-24 00:57:35 +0100

    Add files via upload

---
+++ ex47/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setuptools
+except ImportError:
+    from distutils.core import setup
+
+setup(description='Learn Python then Hard Way - Ex 47',
+    author='Gabriel Ionescu',
+    url='',
+    download_url='',
+    author_email='ionescuig@yahoo.com',
+    version='1.0',
+    install_requires=['nose'],
+    packages=['ex47'],
+    scripts=[],
+    name='ex47'
+    )
